bfef01a11ff47df1826357fed2df1b411bbd4296
Christophe DAVID <49477099+christophe-david@users.noreply.github.com>
2022-04-27 08:31:32 +0200

    Reorganisation

--- types_and_arrays/compile.sh
+++ types_and_arrays/compile.sh
@@ -2,10 +2,10 @@
 # Compilation script for Linux and macOS
 
 # For pre-building pyf file (before modification):
-#   python -m numpy.f2py -m demo -h demo.pyf python_data_reader.f90 python_data_writer.f90 data_bridge.f90 main.f90
+#   python -m numpy.f2py -m demo -h demo.pyf fortran/python_data_reader.f90 fortran/python_data_writer.f90 fortran/data_bridge.f90 fortran/main.f90
 
 
 rm -f *.o *.mod *.so
 
 export LIBRARY_PATH="$LIBRARY_PATH:/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib" # fix for recent macOS from https://stackoverflow.com/a/65428700/16488238
-python -m numpy.f2py -c -m demo demo.pyf types.f90 dummy_data.f90 python_data_reader.f90 python_data_writer.f90 data_bridge.f90 main.f90
+python -m numpy.f2py -c -m demo demo.pyf fortran/types.f90 fortran/dummy_data.f90 fortran/python_data_reader.f90 fortran/python_data_writer.f90 fortran/data_bridge.f90 fortran/main.f90
