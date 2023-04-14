520243857338358e25eaf000d2975a60ae8e0412
Christophe DAVID <christophe.david@onera.fr>
2023-04-14 14:49:51 +0200

    Updated meson build. C wrapper generated on-the-fly.

--- fib_tuto/04_meson_build/compile.sh
+++ fib_tuto/04_meson_build/compile.sh
@@ -1,7 +1,6 @@
 # Generating C wrapper
-python -m numpy.f2py ../01_quick/fib1.f -m fib2 --lower
 rm -rf builddir
 meson setup builddir
 meson compile -C builddir
 cd builddir
-python -c "import numpy as np; import fib2; a = np.zeros(9); fib2.fib(a); print (a)"
+python -c "import numpy as np; import fibby; a = np.zeros(9); fibby.fib(a); print (a)"
